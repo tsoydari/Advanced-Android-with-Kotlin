9674562e860d2239923872d1cf25cdc4f0a4e750
Tsoy Darya <tsoydari@gmail.com>
2020-12-04 09:04:26 +0300

    Override performClick

--- CustomFanController/app/src/main/java/com/example/customfancontroller/DialView.kt
+++ CustomFanController/app/src/main/java/com/example/customfancontroller/DialView.kt
@@ -13,6 +13,13 @@ private enum class FanSpeed(val label: Int) {
     LOW(R.string.fan_low),
     MEDIUM(R.string.fan_medium),
     HIGH(R.string.fan_high);
+
+    fun next() = when (this) {
+        OFF -> LOW
+        LOW -> MEDIUM
+        MEDIUM -> HIGH
+        HIGH -> OFF
+    }
 }
 
 private const val RADIUS_OFFSET_LABEL = 30
@@ -33,6 +40,10 @@ class DialView @JvmOverloads constructor(
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create( "", Typeface.BOLD)
+    }
+
+    init {
+        isClickable = true
     }
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
@@ -62,6 +73,16 @@ class DialView @JvmOverloads constructor(
             canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
         }
 
+    }
+
+    override fun performClick(): Boolean {
+        if (super.performClick()) return true
+
+        fanSpeed = fanSpeed.next()
+        contentDescription = resources.getString(fanSpeed.label)
+
+        invalidate()
+        return true
     }
 
     private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
