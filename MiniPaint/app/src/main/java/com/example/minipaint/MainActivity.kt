38171a440b41b3acdf63a6ed3d2c32307e4dc3dc
Tsoy Darya <tsoydari@gmail.com>
2020-12-07 21:31:08 +0300

    Exercise: Override onSizeChanged()

--- MiniPaint/app/src/main/java/com/example/minipaint/MainActivity.kt
+++ MiniPaint/app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.example.minipaint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import android.view.Window
 import android.view.WindowInsets
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
 
-        val paintCanvasView = PaintCanvasView(this)
+        val paintCanvasView = PaintCanvasView(this )
+
 //        paintCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         paintCanvasView.windowInsetsController?.hide(WindowInsets.Type.navigationBars())
         paintCanvasView.contentDescription = getString(R.string.canvasContentDescription)
--- MiniPaint/app/src/main/java/com/example/minipaint/paintCanvasView.kt
+++ MiniPaint/app/src/main/java/com/example/minipaint/paintCanvasView.kt
@@ -1,7 +1,27 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class PaintCanvasView(context: Context) : View(context) {
+
+    private var extraCanvas: Canvas? = null
+    private var extraBitmap: Bitmap? = null
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+        extraBitmap?.run {
+            this.recycle()
+        }
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraBitmap?.run {
+            extraCanvas = Canvas(this)
+        }
+        extraCanvas?.drawColor(backgroundColor)
+    }
+
 }
