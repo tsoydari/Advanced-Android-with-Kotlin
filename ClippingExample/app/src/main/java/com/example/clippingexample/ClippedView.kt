8d74828735f1110ca4eb6ce0b9684ef9d499e82f
Tsoy Darya <tsoydari@gmail.com>
2020-12-09 00:02:09 +0300

    Exercise 10: override onDraw()

--- ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
+++ ClippingExample/app/src/main/java/com/example/clippingexample/ClippedView.kt
@@ -1,6 +1,9 @@
 package com.example.clippingexample
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,4 +12,61 @@ class ClippedView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
+
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+    }
+    private fun drawDifferenceClippingExample(canvas: Canvas) {
+    }
+    private fun drawCircularClippingExample(canvas: Canvas) {
+    }
+    private fun drawIntersectionClippingExample(canvas: Canvas) {
+    }
+    private fun drawCombinedClippingExample(canvas: Canvas) {
+    }
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas) {
+    }
+    private fun drawOutsideClippingExample(canvas: Canvas) {
+    }
+    private fun drawTranslatedTextExample(canvas: Canvas) {
+    }
+    private fun drawSkewedTextExample(canvas: Canvas) {
+    }
+    private fun drawQuickRejectExample(canvas: Canvas) {
+    }
 }
