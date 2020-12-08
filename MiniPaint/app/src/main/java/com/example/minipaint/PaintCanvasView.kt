26d64ebd27adda9c49e26246b8d68ebe68541fc5
Tsoy Darya <tsoydari@gmail.com>
2020-12-08 22:53:45 +0300

    Exercise 12: Override onTouchEvent()

--- MiniPaint/app/src/main/java/com/example/minipaint/PaintCanvasView.kt
+++ MiniPaint/app/src/main/java/com/example/minipaint/PaintCanvasView.kt
@@ -1,11 +1,17 @@
 package com.example.minipaint
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f // has to be float
 
 class PaintCanvasView @JvmOverloads constructor(
         context: Context,
@@ -17,6 +23,25 @@ class PaintCanvasView @JvmOverloads constructor(
     private var extraCanvas: Canvas = Canvas(extraBitmap)
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+
+    private var path = Path()
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
@@ -31,5 +56,42 @@ class PaintCanvasView @JvmOverloads constructor(
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap, 0f, 0f, null)
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        super.onTouchEvent(event)
+
+        event?.run {
+            motionTouchEventX = x
+            motionTouchEventY = y
+        }
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> {
+                touchUp()
+                performClick()
+            }
+        }
+
+        return true
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
+    private fun touchUp() {
+        TODO("Not yet implemented")
+    }
+
+    private fun touchMove() {
+        TODO("Not yet implemented")
+    }
+
+    private fun touchStart() {
+        TODO("Not yet implemented")
+    }
+
 
 }
